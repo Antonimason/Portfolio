860dbb418e2fd2c9aa1f941d33a820a9977c3dd3
Antonimason <elchuli3000@gmail.com>
2022-08-02 12:46:50 +0100

    se agregan sliders en portfolio

--- funciones.js
+++ funciones.js
@@ -1,4 +1,6 @@
 "use strict"
+
+/*-------------MENU BAR--------------------- */
 document.querySelector(".icon-menu").addEventListener("click", ()=>{
     document.querySelector(".lista").classList.toggle("show");
 });
@@ -13,9 +15,45 @@ let secciones = [seccion1,seccion2,seccion3,seccion4];
 
 secciones.forEach(seccion => seccion.addEventListener("click", ()=>{ document.querySelector(".lista").classList.toggle("show"); })); 
 
+
+/*--------------SCROLL REVEAL------------------- */
 ScrollReveal().reveal(".banner", { delay: 500 });
 ScrollReveal().reveal(".imagenyo", { delay: 500 });
 ScrollReveal().reveal(".yo", { delay: 1000 });
 ScrollReveal().reveal(".about-contenedor", { delay: 500 });
 ScrollReveal().reveal(".tema", { delay: 500 });
 ScrollReveal().reveal(".estudios", { delay: 1500 });
+
+
+/*------------SLIDER------------ */
+(function() {
+    const sliders = [...document.querySelectorAll('.slider_body')];
+    const arrownext = document.querySelector('#after');
+    const arrowbefore = document.querySelector('#before');
+    let value;
+
+    setInterval(()=>{
+        for(let i = 0; i<=2; i++){
+            changePosition(1);
+        }
+    },5000);
+
+    arrownext.addEventListener('click', ()=> changePosition(1));
+
+    arrowbefore.addEventListener('click', ()=> changePosition(-1));
+
+    function changePosition(change) {
+        const currentElement = Number(document.querySelector('.slider_body--show').dataset.id); //obtener el elemento actual con su dataset
+    
+        value = currentElement;
+        value += change; //al valor del data set actual le sumes el valor de los arrow
+        if (value == 0 || value == sliders.length+1) { //slider
+            value = value === 0 ? sliders.length : 1; 
+        }
+
+        sliders[currentElement-1].classList.toggle('slider_body--show');
+        sliders[value-1].classList.toggle('slider_body--show');
+    }
+})
+
+();
