1aeb6378564d9ccc2e36628164e6be943dad3efe
Antonimason <elchuli3000@gmail.com>
2023-01-21 21:21:56 +0000

    se agrega nueva pagina web y barra de progreso

--- funciones.js
+++ funciones.js
@@ -22,7 +22,71 @@ ScrollReveal().reveal(".imagenyo", { delay: 500 });
 ScrollReveal().reveal(".yo", { delay: 500 });
 ScrollReveal().reveal(".about-contenedor", { delay: 500 });
 ScrollReveal().reveal(".tema", { delay: 500 });
-ScrollReveal().reveal(".estudios", { delay: 500 });
+
+/*--------------------INTERSECTION OBSERVER------------- */
+
+
+const studies = document.querySelector(".estudios");
+
+const options = {
+    threshold: 0.3
+  }
+
+const verifyVisibility = (entries) => {
+    const entry = entries[0];
+    console.log(entry);
+
+    if (entry.isIntersecting) {
+
+    let htmlLoad = document.querySelector('.html-load')
+    htmlLoad.style.animationName = "going100";
+
+    let cssLoad = document.querySelector('.css-load')
+    cssLoad.style.animationName = "going100";
+
+    let jsLoad = document.querySelector('.js-load')
+    jsLoad.style.animationName = "going80";
+
+    let bootsLoad = document.querySelector('.bootstrap-load')
+    bootsLoad.style.animationName = "going50";
+
+    let jqueryLoad = document.querySelector('.jquery-load')
+    jqueryLoad.style.animationName = "going70";
+
+    let reactLoad = document.querySelector('.react-load')
+    reactLoad.style.animationName = "going70";
+
+
+    
+    
+    let contar = (barra, elemento, porcentaje) => {
+        let cuenta = 0;
+        setInterval(() => {
+            if(cuenta == porcentaje) {
+                clearInterval();
+            } else {
+                cuenta = cuenta + 1;
+                elemento.innerHTML = cuenta + "%";
+                barra.style.width = `${cuenta}%`;
+            }
+        }, 20);
+    }
+
+    contar(htmlLoad, document.querySelector('#htmlNumber'), 100);
+    contar(cssLoad, document.querySelector('#cssNumber'), 100);
+    contar(jsLoad, document.querySelector('#jsNumber'), 80);
+    contar(bootsLoad, document.querySelector('#bootsNumber'), 50);
+    contar(jqueryLoad, document.querySelector('#jqueryNumber'), 70);
+    contar(reactLoad, document.querySelector('#reactNumber'), 70);
+}
+    
+
+    
+}
+const observer = new IntersectionObserver(verifyVisibility, options);
+
+observer.observe(studies);
+
 
 
 /*------------SLIDER------------ */
